6b7a1a85b46fab435bbea54627b14a8efbc5fd19
azzaoalloush <azzaoalloush@gmail.com>
2026-02-22 11:16:46 +0800

    Changed information from previous user to current user

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const resumeData = {
   personal: {
-    name: "Tashanda Chealsy A. Antonio",
-    birthDate: "August 7, 2005",
-    birthplace: "Tuguegarao City, Cagayan",
+    name: "Azza M. Alloush",
+    birthDate: "April 9, 2006",
+    birthplace: "Abu Dhabi, United Arab Emirates",
     gender: "Female",
     citizenship: "Filipino",
-    religion: "Roman Catholic",
+    religion: "Islam",
     address: "Tuguegarao City, Cagayan",
-    email: "tashandaantonio@spup.edu.ph"
+    email: "azzaalloush@spup.edu.ph"
   },
   education: {
     degree: "Bachelor of Science in Information Technology",
@@ -15,24 +15,20 @@ const resumeData = {
     years: "2023 - Present",
     capstone: "Dynamic Door Security using IoT Application",
     shs: { school: "St. Paul University Philippines", years: "2021 - 2023" },
-    jhs: { school: "St. Catherine's Academy of Gattaran, Inc.", years: "2017 - 2021" }
+    jhs: { school: "Vision Private School", years: "2017 - 2021" }
   },
   certifications: [
-    { title: "Certificate of Recognition, Pelikulang Panlipunan Film Festival 2026", org: "St. Paul University Philippines", date: "January 17, 2026", desc: "Recognized for outstanding participation in the Pelikulang Panlipunan Film Festival 2026." },
-    { title: "Certificate of Attendance, JPCS Leadership Transformation Workshop & NBO Election AY 2025-2026", org: "Junior Philippine Computer Society (JPCS)", date: "December 9, 2025", desc: "Attendance certificate for the JPCS Leadership Transformation Workshop and NBO Election for the academic year 2025-2026." },
-    { title: "Certificate of Academic Excellence, Second Semester, AY 2025-2026", org: "St. Paul University Philippines", date: "January 2026", desc: "Awarded for outstanding academic performance in the second semester of the academic year 2025-2026." },
-    { title: "Certificate of Attendance, PCS Monthly Membership Meeting and Techtalks 2025", org: "Philippine Computer Society", date: "October 23, 2025", desc: "Attendance certificate for the monthly membership meeting and tech talks organized by the Philippine Computer Society." },
-    { title: "Certificate of Attendance, PCS Monthly Membership Meeting and Techtalks 2025", org: "Philippine Computer Society", date: "November 27, 2025", desc: "Attendance certificate for the monthly membership meeting and tech talks organized by the Philippine Computer Society." },
-    { title: "Certificate of Membership, Philippine Computer Society", org: "Philippine Computer Society", date: "January 15, 2026", desc: "Membership certificate for the Philippine Computer Society for the academic year 2025-2026." },
     { title: "Certificate of Membership, JPCS-SPUP Chapter 2026", org: "Junior Philippine Computer Society (JPCS)", date: "January 15, 2026", desc: "Membership certificate for the JPCS-SPUP Chapter for the academic year 2025-2026." },
-    { title: "Certificate of Recognition, SPUP Paskuhan 2025 PaulInnovate", org: "St. Paul University Philippines", date: "December 2025", desc: "Recognized for outstanding participation in the PaulInnovate event during SPUP Paskuhan 2025." },
+    { title: "Certificate of Membership, Philippine Computer Society", org: "Philippine Computer Society", date: "January 15, 2026", desc: "Membership certificate for the Philippine Computer Society for the academic year 2025-2026." },
+    { title: "Certificate of Recognition, Pelikulang Panlipunan Film Festival 2026", org: "St. Paul University Philippines", date: "January 17, 2026", desc: "Recognized for outstanding participation in the Pelikulang Panlipunan Film Festival 2026." },
     { title: "Certificate of Attendance, AI Hygiene: Team Norms That Stop Workslop Before It Starts 2025", org: "Eskwelabs", date: "October 29, 2025", desc: "A workshop on AI hygiene and team norms to prevent workslop." },
-    { title: "Certificate of Recognition, IT Cybersecurity Roadshow 2025", org: "St. Paul University Philippines", date: "October 25, 2025", desc: "Recognized for helping organize the event." },
     { title: "Certificate of Participation, IT Cybersecurity Roadshow 2025", org: "St. Paul University Philippines", date: "October 25, 2025", desc: "A roadshow event focused on raising awareness and promoting best practices in IT cybersecurity for students and faculty." },
     { title: "Certificate of Participation, The Art of Filmmaking Workshop 2025", org: "St. Paul University Philippines", date: "April 21, 2025", desc: "A workshop that taught students the fundamentals of filmmaking and storytelling." },
-    { title: "Certificate of Participation, SITE Film Festival 2025", org: "St. Paul University Philippines", date: "June 19, 2025", desc: "An annual film festival organized by the St. Paul University Philippines to showcase student filmmaking skills and promote storytelling." },
-    { title: "Certificate of Participation, ITE CONVENTION 2025", org: "St. Paul University Philippines", date: "March 26-28, 2025", desc: "An annual convention for Information Technology students and professionals to share knowledge and network." }
-  ],
+    { title: "Certificate of Recognition, SITE Film Festival 2025", org: "St. Paul University Philippines", date: "June 19, 2025", desc: "An annual film festival organized by the St. Paul University Philippines to showcase student filmmaking skills and promote storytelling." },
+    { title: "Certificate of Participation, SITE Film Festival 2025", org: "St. Paul University Philippines", date: "June 19, 2025", desc: "An annual film festival organized by the St. Paul University Philippines to showcase student filmmaking skills and promote storytelling." },{ title: "Certificate of Recognition, Pelikulang Panlipunan Film Festival 2026", org: "St. Paul University Philippines", date: "January 17, 2026", desc: "Recognized for outstanding participation in the Pelikulang Panlipunan Film Festival 2026." },
+    { title: "Certificate of Participation, ITE CONVENTION 2025", org: "St. Paul University Philippines", date: "March 26-28, 2025", desc: "An annual convention for Information Technology students and professionals to share knowledge and network." },
+    { title: "Certificate of Participation, ITE CONVENTION 2024", org: "St. Paul University Philippines", date: "April 17-19, 2024", desc: "An annual convention for Information Technology students and professionals to share knowledge and network." }
+    ],
   events: [
     { 
       title: "Pelikulang Panlipunan Film Festival 2026", 
@@ -40,20 +36,6 @@ const resumeData = {
       date: "January 17, 2026", 
       desc: "A film festival that showcases the film making skills of students in St. Paul University Philippines, with a focus on social issues and community stories.",
       img: "://picsum.photos/httpsseed/pelpan/600/400"
-    },
-    { 
-      title: "JPCS Leadership Transformation Workshop & NBO Election AY 2025-2026", 
-      venue: "Online via Zoom", 
-      date: "December 9, 2025", 
-      desc: "A workshop organized by the Junior Philippine Computer Society (JPCS) to train and prepare student leaders for their roles in the upcoming academic year, including the election of new officers.",
-      img: "://picsum.photos/httpsseed/jpcs/600/400"
-    },
-    { 
-      title: "PCS Monthly Membership Meeting and Techtalks 2025", 
-      venue: "Online via Zoom", 
-      date: "November 27, 2025", 
-      desc: "Monthly meeting and tech talks organized by the Philippine Computer Society (PCS) for members to stay updated on industry trends and network with fellow professionals.",
-      img: "://picsum.photos/httpsseed/pcs/600/400"
     },
     { 
       title: "AI Hygiene: Team Norms That Stop Workslop Before It Starts 2025", 
@@ -68,13 +50,6 @@ const resumeData = {
       date: "October 25, 2025", 
       desc: "A roadshow event focused on raising awareness and promoting best practices in IT cybersecurity for students and faculty.",
       img: "://picsum.photos/httpsseed/itcybersecurity/600/400"
-    },
-    { 
-      title: "PCS Monthly Membership Meeting and Techtalks 2025", 
-      venue: "Online via Zoom", 
-      date: "October 23, 2025", 
-      desc: "Monthly meeting and tech talks organized by the Philippine Computer Society (PCS) for members to stay updated on industry trends and network with fellow professionals.",
-      img: "://picsum.photos/httpsseed/pcs/600/400"
     },
     { 
       title: "SITE Film Festival 2025", 
@@ -107,22 +82,10 @@ const resumeData = {
   ],
   affiliations: [
     { 
-      role: "The Browser - Managing Editor", 
-      organization: "SPUP Student Publications", 
-      period: "2025-2026",
-      desc: "Oversaw editorial content, coordinated with writers and designers, and ensured timely publication of the university's student newspaper"
-    },
-    { 
       role: "JPCS - Member", 
       organization: "Junior Philippine Computer Society", 
       period: "2025-2026",
       desc: "Active member in Philippines' premier IT professional organization, attending networking events and technical workshops"
-    },
-    { 
-      role: "JPCS-SPUP Director for Membership", 
-      organization: "Junior Philippine Computer Society - SPUP Chapter", 
-      period: "2025-2026",
-      desc: "Led membership recruitment and engagement initiatives, organized events to foster community among IT students at SPUP"
     },
     { 
       role: "Philippine Computer Society Member", 
